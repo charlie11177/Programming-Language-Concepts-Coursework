82a6970c751ec823ee617badcf65ad7249bed626
cg4g19 <cg4g19@soton.ac.uk>
2021-04-27 18:31:21 +0100

    Created processFile function

--- Eval.hs
+++ Eval.hs
@@ -1,7 +1,21 @@
 --module Eval --where
 --import Grammar
-
+import Data.List
 
 type Table = [(String, Int, [String])]
 
---processFile :: CSVFile ->
+processFile :: String -> IO Table
+processFile path = do
+  sourceText <- readFile path
+  let name = take (length path - 4) path
+  let cols = splitCols sourceText
+  return ([(name, fst x, snd x) | x <- zip [0..] cols])
+
+splitCols :: String -> [[String]]
+splitCols xs = transpose (map (splitOn ',') (splitOn '\n' xs))
+
+splitOn :: Char -> String -> [String]
+splitOn c [] = [""]
+splitOn c (x:[]) | x == c = [[]]
+splitOn c (x:xs) | x == c = "":(splitOn c xs)
+splitOn c (x:xs) | otherwise = (x : head (splitOn c xs)):(tail $ splitOn c xs)
